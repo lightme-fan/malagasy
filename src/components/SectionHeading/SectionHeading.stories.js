dc2dac28533aaf59f9152513484f8812199efc6d
student <Fanilo>
2021-04-27 21:10:54 +0300

    Added comment

--- src/components/SectionHeading/SectionHeading.stories.js
+++ src/components/SectionHeading/SectionHeading.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {storiesOf} from '@storybook/react-native';
 import SectionHeading from './SectionHeading';
 
+// Some changes
 storiesOf('Section Heading', module).add('Heading', () => (
   <SectionHeading title={'Select a category:'} />
 ));
