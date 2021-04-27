09bb67aeb18f1c6f4bd62406cc20cee045e4ce57
student <Fanilo>
2021-04-27 03:22:56 +0300

    running test

---
+++ src/components/SectionHeading/SectionHeading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const SectionHeading = ({title}) => {
+  return <Text>{title}</Text>;
+};
+
+export default SectionHeading;
---
+++ src/components/SectionHeading/SectionHeading.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+import SectionHeading from './SectionHeading';
+
+storiesOf('', module).add('', <SectionHeading />);
