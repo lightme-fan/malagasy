cce91035796d709c53f0432424c15b7a9591f230
student <Fanilo>
2021-04-28 03:09:55 +0300

    Refactored list component

--- src/components/List/List.stories.js
+++ src/components/List/List.stories.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react-native';
+import {View, StyleSheet} from 'react-native';
 
 import List from './List';
+import ListItem from '../ListItem/ListItem';
+import ActionButton from '../ActionButton/ActionButton';
 import DATA from '../../data/categories.json';
 
+const styles = StyleSheet.create({
+  listItem: {
+    borderWidth: 1,
+    borderColor: '#E5E5E5',
+    fontSize: 16,
+    lineHeight: 19,
+    color: '#111827',
+  },
+});
+
+function DisplayData({actionText, actionStyle, onPressButton}) {
+  return (
+    <View>
+      {DATA.categories.map((item, index) => (
+        <ListItem
+          category={item.name.en}
+          key={item.id}
+          style={styles.listItem}
+          borderWidth={1}
+          borderBottomWidth={0.4}
+          borderColor={'#E5E5E5'}
+          multiline={true}>
+          <ActionButton
+            text={actionText}
+            textStyle={actionStyle}
+            onPress={onPressButton}
+          />
+        </ListItem>
+      ))}
+    </View>
+  );
+}
+
 storiesOf('List', module).add('List categories', () => (
-  <List
-    data={DATA.categories}
-    actionText={'Learn'}
-    actionStyle={'#06B6D4'}
-    onPressButton={() => alert('Clicked button')}
-  />
+  <List>
+    <DisplayData
+      actionText={'Learn'}
+      actionStyle={'#06B6D4'}
+      onPressButton={() => alert('Clicked button')}
+    />
+  </List>
 ));
